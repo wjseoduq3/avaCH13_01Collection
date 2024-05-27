4a06ecfdc17bf3151f6891be88a3b60d19149d36
wjseoduq3 <dragon65@nate.com>
2024-05-27 16:06:10 +0900

    4'th commit

--- src/com/jdy/ch13_02Board/BoardTestExample.java
+++ src/com/jdy/ch13_02Board/BoardTestExample.java
@@ -13,9 +13,7 @@ public class BoardTestExample {
 		boardList.add(new BoardTest("주제3","내용3","글쓴이3"));
 		boardList.add(new BoardTest("주제4","내용4","글쓴이4"));
 		boardList.add(new BoardTest("주제5","내용5","글쓴이5"));
-		
-		
-		
+				
 	}
 
 }
